6d307594a9cb5da70ce37e5c4d148490699bfcc0
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-04-30 16:51:39 +0100

    Add test passing

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    private Calculator testCalculator;
+    @Before
+    public void before() {
+        testCalculator = new Calculator(2, 3);
+    }
+    @Test
+    public void add(){
+        assertEquals(5,testCalculator.add());}
+
+
 }
